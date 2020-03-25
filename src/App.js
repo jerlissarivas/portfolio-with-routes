9bd94de37f20a77a4fdb885daa6b94a16df28f83
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-03-25 00:02:47 -0400

    Complete React Router Lessons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import {projects as Projects} from './components/Projects';
+import ProjectDetails from './components/ProjectDetails';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route exact path='/projects' component={Projects}/>
+          <Route exact path="/projects/:id" component={ProjectDetails} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
